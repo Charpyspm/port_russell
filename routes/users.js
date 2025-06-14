ee668310c2a8e3f5967f44ea09dc6835d269fb55
Charpyspm <mcharpentier83@icloud.com>
2025-06-14 14:02:13 +0200

    création de l'authetification

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,16 @@ const service = require('../services/users');
 
 router.get('/:id', service.getById);
 
-router.put('/add', service.add);
+router.post('/add', service.add);
 
 router.patch('/:id', service.update);
 
 router.delete('/:id', service.delete);
 
+// ajout de la route /authenticate
+router.post('/authenticate', service.authenticate);
+
 module.exports = router;
+
+
+// to do : mettre en place la fonction de login
