f452f7fe5fda9d1080afc07769cec3c7fc2b0dea
Charpyspm <mcharpentier83@icloud.com>
2025-06-14 11:49:41 +0200

    creation des routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const userRoute = require('../routes/users');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async(res, req) => {
+    req.status(200).json({
+      name: proccess.env.APP_NAME,
+      version : process.env.APP_VERSION,
+      status: 200,
+      message: 'Welcome to PortRussell API',
+    })
 });
 
 module.exports = router;
