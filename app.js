ed08d0d8a055beed9a7575af23c4068e087fac18
Charpyspm <mcharpentier83@icloud.com>
2025-06-14 10:23:39 +0200

    connexion a mongoDB

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// connexion mongodb
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://devoirportrussell:3OOjynuIp6ACF2A9@portrussell.itav1us.mongodb.net/?retryWrites=true&w=majority&appName=PortRussell";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
